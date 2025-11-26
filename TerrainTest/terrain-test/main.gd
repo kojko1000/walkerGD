af72c0f37d9df9f2dfc1917dcf71945431f1016f
kojko1000 <122826517+kojko1000@users.noreply.github.com>
2025-11-26 21:51:25 +0300

    timeChanges

--- TerrainTest/terrain-test/main.gd
+++ TerrainTest/terrain-test/main.gd
@@ -25,9 +25,9 @@ func _on_timer_timeout() -> void:
 		sun.shadow_enabled = false
 		#World_Environment.ambient_light_energy -=light_intensity*0.1
 		if  sky.environment.background_energy_multiplier>0.1:
-			sky.environment.background_energy_multiplier =sky.environment.background_energy_multiplier- 0.01
-		if sky.environment.ambient_light_energy > 0.05:
-			sky.environment.ambient_light_energy = sky.environment.ambient_light_energy - 0.01
+			sky.environment.background_energy_multiplier =sky.environment.background_energy_multiplier- 0.001
+		if sky.environment.ambient_light_energy > 0.25:
+			sky.environment.ambient_light_energy = sky.environment.ambient_light_energy - 0.001
 		#ambient_light_energy
 		print("заходим")
 	if((rad_to_deg(sun.rotation.x)>168 or rad_to_deg(sun.rotation.x)< 8)): #восход 168
@@ -35,8 +35,8 @@ func _on_timer_timeout() -> void:
 			sun.light_energy = sun.light_energy + light_intensity
 		sun.shadow_enabled = true
 		if  sky.environment.background_energy_multiplier<1.2:
-			sky.environment.background_energy_multiplier =sky.environment.background_energy_multiplier+ 0.01
+			sky.environment.background_energy_multiplier =sky.environment.background_energy_multiplier+ 0.001
 		if sky.environment.ambient_light_energy < 1:
-			sky.environment.ambient_light_energy = sky.environment.ambient_light_energy + 0.01
+			sky.environment.ambient_light_energy = sky.environment.ambient_light_energy + 0.001
 		print("восходим")
 	pass # Replace with function body.
